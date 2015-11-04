c26a421298f893e14edb820163fca2ec498fa58a
Jon Harlem <jtharlem@gmail.com>
2015-11-04 12:53:19 -0800

    random colored divs

--- script.js
+++ script.js
@@ -4,17 +4,22 @@ body.style.margin = "0";
 
 var i = 0;
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 while (i<81) {
 	var square = document.createElement('div');
 	square.style.width = "11.11%";
 	square.style.float = "left";
 	square.style.margin = "0";
 	square.style.paddingBottom = "11.11%";
-	if (i % 2 === 0) {
-		square.style.backgroundColor = "black";
-	} else {
-		square.style.backgroundColor = "red";
-	}
+	square.style.backgroundColor = getRandomColor();
 	document.body.appendChild(square);
 	i++;
 }
