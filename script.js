72277ed0dfab42f63abe02c559a74d0ae89d3877
Jon Harlem <jtharlem@gmail.com>
2015-11-04 12:41:23 -0800

    checkboard solution

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// Your JS goes here
+var body = document.getElementsByTagName('body')[0];
+
+body.style.margin = "0";
+
+var i = 0;
+
+while (i<81) {
+	var square = document.createElement('div');
+	square.style.width = "11.11%";
+	square.style.float = "left";
+	square.style.margin = "0";
+	square.style.paddingBottom = "11.11%";
+	if (i % 2 === 0) {
+		square.style.backgroundColor = "black";
+	} else {
+		square.style.backgroundColor = "red";
+	}
+	document.body.appendChild(square);
+	i++;
+}
